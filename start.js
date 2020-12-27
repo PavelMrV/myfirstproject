5598e85c0bd8564e1e7842a4d01aa1f38a3f19bd
Pavel <pavel.m.1600@gmail.com>
2020-12-27 19:40:01 +0300

    first patr of second HW

--- start.js
+++ start.js
@@ -11,14 +11,25 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних фильмов?', ' '), 
-      b = +prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних фильмов?', ' '), 
-      d = +prompt('На сколько оцените его?', '');
+//const a = prompt('Один из последних фильмов?', ' '), 
+      //b = +prompt('На сколько оцените его?', ''),
+      //c = prompt('Один из последних фильмов?', ' '), 
+      //d = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+//personalMovieDB.movies[a] = b;
+//personalMovieDB.movies[c] = d;
+
+//console.log(personalMovieDB);
+
+for (let i = 0; i < 2; i++) {
+   const a = prompt('Один из последних фильмов?', ' '), 
+         b = prompt('На сколько оцените его?', '');
+
+    personalMovieDB.movies[a] = b;
+}
 
 console.log(personalMovieDB);
+
+
 
 
