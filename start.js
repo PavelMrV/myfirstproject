8066cedf486d949ae1afc0446a3de8386c9627c5
Pavel <pavel.m.1600@gmail.com>
2020-12-28 21:23:03 +0300

    project part2

--- start.js
+++ start.js
@@ -11,23 +11,32 @@ const personalMovieDB = {
     privat: false
 };
 
-//const a = prompt('Один из последних фильмов?', ' '), 
-      //b = +prompt('На сколько оцените его?', ''),
-      //c = prompt('Один из последних фильмов?', ' '), 
-      //d = +prompt('На сколько оцените его?', '');
 
-//personalMovieDB.movies[a] = b;
-//personalMovieDB.movies[c] = d;
-
-//console.log(personalMovieDB);
 
 for (let i = 0; i < 2; i++) {
    const a = prompt('Один из последних фильмов?', ' '), 
          b = prompt('На сколько оцените его?', '');
 
-    personalMovieDB.movies[a] = b;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i-- // возвращаем цикл на один назад, каждый раз когда получаем ошибку
+    }
+
+   
 }
 
+if (personalMovieDB.count < 10) {
+    console.log('Ты еще молодоый');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log('Ты уже знаешь толк в кино');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Ты задрот кино!');
+} else {
+    console.log('Ошбибка');
+}
 console.log(personalMovieDB);
 
 
